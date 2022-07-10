33244a7e4da1ecd893747d9520e74f909e806de7
Soumwadeep Guha <soumwadeep@gmail.com>
2022-07-10 11:00:07 +0530

    added custom navbar

---
+++ client/src/components/SoumFooter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/SoumFooter.css'
+const SoumFooter = () => {
+  return (
+    <>
+      <footer className="footer">
+        <h1>Thanks For Visiting</h1>
+      </footer>
+    </>
+  )
+}
+
+export default SoumFooter
